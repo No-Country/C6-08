1a6158b77ca954e76c4e1a64330ea7347f33b2c5
mac <mac@MacBook-Pro-de-mac.local>
2022-08-10 15:31:27 -0500

    firts commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const { globalErrorHandler } = require('./controllers/globalErrorhandler')
+const { AppError } = require('./util/AppError')
+const { adminHotelRouter } = require('./routes/adminhotel')
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/v1/adminhotel', adminHotelRouter)
+
+app.use('*', (req, res, next) => {
+    next(new AppError(404, "not found this server."))
+})
+
+app.use(globalErrorHandler)
+
+
+module.exports = { app }
