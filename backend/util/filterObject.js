b5dbde3153d01aa6ea0a5338fbfda6efe4999d83
mac <mac@MacBook-Pro-de-mac.local>
2022-08-10 15:53:23 -0500

    firts commit

--- backend/util/filterObject.js
+++
@@ -1,8 +0,0 @@
-exports.filterObject = (obj, ...allowField) =>{
-    const newObject = {}
-    Object.keys(obj).forEach(el => {
-        if(allowField.includes(el))
-        newObject[el] = obj[el]
-    })
-    return newObject
-}
