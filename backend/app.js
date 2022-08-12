3c532afb93a69be6c1b608568745ef06a2778ce2
Nythero <agustin-ag@live.com>
2022-08-12 17:21:38 +0300

    Se agrego las rutas de los hoteles a la app

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const { globalErrorHandler } = require('./controllers/globalErrorhandler')
 const { AppError } = require('./util/AppError')
 const { adminHotelRouter } = require('./routes/adminhotel')
+const { hotelRouter } = require('./routes/hotel')
 
 const app = express()
 
 app.use(express.json())
 app.use('/api/v1/docs', express.static(__dirname + '/docs'))
 app.use('/api/v1/adminhotel', adminHotelRouter)
+app.use('/api/v1/hoteles', hotelRouter)
 
 app.use('*', (req, res, next) => {
     next(new AppError(404, "not found this server."))
