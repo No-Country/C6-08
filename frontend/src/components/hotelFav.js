a5e62dd2ad2802d0244e9404608e693d1c8230b0
Santiago Gabriel Rossi <santiagogabrielrossi@yahoo.com>
2022-08-15 22:58:50 -0300

    Actualizacion de routes, creacion de components: propiedades y hoteles favoritos

---
+++ frontend/src/components/hotelFav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HotelFav = () => {
+  return (
+      <section className="home">
+        <div id="contenedor">
+          <h1>Hoteles Favoritos:</h1>
+        </div>
+      </section>
+    
+  );
+};
+
+export default HotelFav;
--- frontend/src/components/routes.js
+++ frontend/src/components/routes.js
@@ -1,6 +1,8 @@
 import Home from './Home'
 import Forms from './formularios'
 import HotelDetail from './hotelDetail/HotelDetail'
+import HotelFav from './hotelFav'
+import Propiedad from './propiedad'
 import {
   Routes,
   Route,
@@ -13,6 +15,8 @@ function Public() {
   return (
       <Routes>
         <Route path='/forms' element={<Forms />} />
+        <Route path='/favs' element={<HotelFav />} />
+        <Route path='/propiedad' /*'/propiedad/:id'*/ element={<Propiedad />} />
         <Route path='/' element={<Home />} />
         <Route path='/Detail' /*'/Detail/:id'*/ element={<HotelDetail />} />
       </Routes>
