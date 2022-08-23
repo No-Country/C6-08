b7a755fbcab33b66f3521c8e44dbe2e18813f1b5
Daniel Albanez <danielalbanez40@gmail.com>
2022-08-23 16:17:27 -0300

    Separando seccion home en componentes

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HeroImage from './heroImage';
+import SearchForm from './SearchForm';
+import '../css/home.css';
+
+const Home = () => {
+  return (
+    <>
+      <section className="home">
+        <div className="hero-image">
+          <HeroImage />
+          <SearchForm />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
